85e262c04292a2c30d866166de4d4f093b709cad
Zettroke <skorostnoy2000@yandex.ru>
2021-02-08 07:44:19 +0300

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,37 @@ pub fn kappa() -> u32 {
     1337
 }
 
+struct LocalHeader {
+    pub signature: u32,
+    pub version: u16,
+    pub bit_flag: u16,
+    pub compression: u16,
+    pub modification_time: u16,
+    pub modification_date: u16,
+    pub crc32: u16,
+    pub compressed_size: u32,
+    pub uncompressed_size: u32,
+    pub file_name: String,
+    pub extra_field_length: u16
+}
+
+impl LocalHeader {
+    pub fn write_local_file_header<T: BufMut>(&self, buff: &mut T) {
+        buff.put_u32_le(0x04034b50);
+        buff.put_u16_le(self.version);
+        buff.put_u16_le(0b00000000_00001000);
+        buff.put_u16_le(0);
+        buff.put_u16_le(self.modification_time);
+        buff.put_u16_le(self.modification_date);
+        buff.put_u32_le(0);
+        buff.put_u32_le(0);
+        buff.put_u32_le(0);
+        buff.put_u16_le(self.file_name.len() as u16);
+        buff.put_u16_le(self.extra_field_length);
+        buff.put_slice(self.file_name.as_bytes());
+    }
+}
+
 struct ZipPacker<T> where T: AsMut<dyn Read> {
     buffer: BytesMut,
     files: Vec<(String, T)>
@@ -21,11 +52,12 @@ impl<T> ZipPacker<T> where T: AsMut<dyn Read> {
 
     pub fn add_file<S: Into<String>>(&mut self, name: S, file: T) {
         self.files.push((name.into(), file));
-        self.buffer.put_u128_le()
+        // self.buffer.put_u128_le()
     }
 
     fn fill_out_buff(&mut self) {
-        // self.buffer.put_u32_le()
+
+
     }
 }
 
